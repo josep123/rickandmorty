cab5daa0ed80c74be449347ca3ac602b450a981d
José Luis <joseluis.rodriguez@hotmail.es>
2023-11-05 19:13:17 +0100

    limpieza

--- src/pages/character/index.js
+++ src/pages/character/index.js
@@ -1,32 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import React from 'react';
+
 import FeedCharacters from '../../components/feedCharacters';
 
 function Character() {
-  const { id } = useParams(); // Obtén la 'id' dinámica desde la URL
 
-  const [results, setResults] = useState([]);
-
-
-
-
-  useEffect(() => {
-    // Utiliza la 'id' en la solicitud a la API
-    fetch(`https://rickandmortyapi.com/api/character/${id}`)
-      .then((response) => response.json())
-      .then((data) => {
-        setResults(data); // Actualiza los resultados del personaje
-      })
-      .catch((error) => console.log(error));
-  }, [id]); // Asegúrate de incluir 'id' como dependencia
-
-  
 
   return (
     <div>
       <FeedCharacters />
-
-      {}
     </div>
   );
 }
