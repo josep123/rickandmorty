fcb55fefab636f59ba2cdb7baf6c856c4f771369
José Luis <joseluis.rodriguez@hotmail.es>
2023-11-05 23:01:00 +0100

    retoque

--- src/components/menuCanvas/index.js
+++ src/components/menuCanvas/index.js
@@ -1,5 +1,5 @@
 import React, { useState} from "react";
-import {Offcanvas, Button, Col} from 'react-bootstrap';
+import {Offcanvas, Button} from 'react-bootstrap';
 
 
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Row, Col, Button} from 'react-bootstrap';
-import Feedcharacters from '../../components/feedCharacters';
+
 
 
 
